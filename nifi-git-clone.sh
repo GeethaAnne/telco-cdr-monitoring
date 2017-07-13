e2fa68683ccfe757d5472f0815c1f52757ff9472
geetha-hortonworks <ganne@hortonworks.com>
2017-07-13 10:31:55 -0700

    git clone removed $VERSION

--- nifi-git-clone.sh
+++ nifi-git-clone.sh
@@ -1,1 +1,1 @@
-git clone ""https://github.com/abajwa-hw/ambari-nifi-service.git"" ""/var/lib/ambari-server/resources/stacks/HDP/$VERSION/services/NIFI""
+git clone https://github.com/abajwa-hw/ambari-nifi-service.git /var/lib/ambari-server/resources/stacks/HDP/2.6/services/NIFI
